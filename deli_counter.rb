163221eab90609eeea6aea3ffa30b5bb88654cd1
Austin Rhoads <austinrrhoads@ymail.com>
2020-02-05 19:33:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,16 @@
 # Write your code here.
 def line(array)
-if array.count == 0
-  puts "The line is currently empty."
-else
-
-end
+  ary = []
+  if array.count == 0
+    puts "The line is currently empty."
+  else
+    array.each_with_index do |name, index|
+    num = index + 1
+    ary.push("#{num}. name")
+    end
+    puts "The line is currently: " + ary.join(" ")
+    
+  end
 end
 
 def take_a_number(array)
